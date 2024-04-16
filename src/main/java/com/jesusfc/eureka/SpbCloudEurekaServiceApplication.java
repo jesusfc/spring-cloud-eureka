f971cabb9ca8ad918c1bee96a15dc3cbc361fdb6
Jesusfc <jfcaraballo@gmail.com>
2024-04-16 16:39:50 +0200

    update microservice architecture - Eureka service and more

---
+++ src/main/java/com/jesusfc/eureka/SpbCloudEurekaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jesusfc.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbCloudEurekaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpbCloudEurekaServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jesusfc/comjesusfceurekaservice/ComJesusfcEurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jesusfc.comjesusfceurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComJesusfcEurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
