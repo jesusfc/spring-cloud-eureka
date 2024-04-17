ef26a775c428446dc26fc80051fd28350d0d2a82
jfcaraballo <jfcaraballo@gmail.com>
2024-04-17 13:36:45 +0200

    update project for Eureka Server

--- src/test/java/com/jesusfc/eureka/SpbCloudEurekaServiceApplicationTests.java
+++ src/test/java/com/jesusfc/eureka/SpbCloudEurekaServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.jesusfc.comjesusfceurekaservice;
+package com.jesusfc.eureka;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ComJesusfcEurekaServiceApplicationTests {
+class SpbCloudEurekaServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
